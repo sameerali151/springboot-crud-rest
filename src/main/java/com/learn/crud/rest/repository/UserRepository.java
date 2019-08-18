b12de392cc9695b715804674041cba4935dbaa7a
sameer <syedjava28@gmail.com>
2019-08-18 06:56:00 -0400

    crud rest

---
+++ src/main/java/com/learn/crud/rest/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.learn.crud.rest.repository;
+
+import com.learn.crud.rest.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
